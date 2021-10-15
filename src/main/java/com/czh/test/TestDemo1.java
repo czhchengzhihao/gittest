ad474f218e4ae3a3bd0b02ae93c0f0c56c70b82e
channing.cheng <channing.cheng@dbappsecurity.com.cn>
2021-10-15 09:34:17 +0800

    测试git分支

---
+++ src/main/java/com/czh/test/TestDemo1.java
@@ -0,0 +1,16 @@
+package com.czh.test;
+
+/**
+ * @PackageName: com.czh.test
+ * @ClassName: TestDemo1
+ * @Description: TestDemo1/description:
+ * @Author: ChengZhiHao
+ * @Date: 2021/10/15 9:33
+ * @Version: v1.0
+ */
+public class TestDemo1 {
+
+    public static void main(String[] args) {
+        System.out.println("这是创建的分支chengzhihao");
+    }
+}
