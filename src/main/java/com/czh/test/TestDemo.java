f17834e5a4977b9f4ed0cc069d6079b9cb496c5f
channing.cheng <channing.cheng@dbappsecurity.com.cn>
2021-10-15 09:29:03 +0800

    测试git

---
+++ src/main/java/com/czh/test/TestDemo.java
@@ -0,0 +1,15 @@
+package com.czh.test;
+
+/**
+ * @PackageName: com.czh.test
+ * @ClassName: TestDemo
+ * @Description: TestDemo/description:
+ * @Author: ChengZhiHao
+ * @Date: 2021/10/15 9:28
+ * @Version: v1.0
+ */
+public class TestDemo {
+    public static void main(String[] args) {
+        System.out.println("这是主分支");
+    }
+}
